a8be4b28a9e2d58bc9a0d6c3d7eb36c0407e7cf7
Mito6211 <willmoore674@gmail.com>
2021-03-14 15:52:30 -0400

    Added navbar, styling improvements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Container, Flex, Text, Box, Button, Textarea } from "@chakra-ui/react";
+import {
+  Container,
+  Flex,
+  Text,
+  Box,
+  Button,
+  Textarea,
+  Heading,
+  Spacer,
+} from "@chakra-ui/react";
 
 export default function App() {
   const [note, setNote] = useState(localStorage.getItem("note") || "");
@@ -13,9 +22,17 @@ export default function App() {
   }, [note, allNotes]);
 
   return (
-    <Container centerContent maxW="1000px">
+    <Container maxW="1000px">
+      <Flex justify="center" align="center" my="20px">
+        <Box p="2">
+          <Heading size="lg">My Notes</Heading>
+        </Box>
+        <Spacer />
+        <Button colorScheme="orange">View Notes</Button>
+      </Flex>
       <Flex direction="column">
         <Textarea
+          minH="300px"
           placeholder="Add a note here!"
           name="note"
           value={note}
@@ -26,6 +43,8 @@ export default function App() {
             setAllNotes((prev) => [...prev, note]);
             setNote("");
           }}
+          colorScheme="orange"
+          my="10px"
         >
           Add Note
         </Button>
