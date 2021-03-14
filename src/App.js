058e35b87b3106df4fe367e893a1a0b5892e5496
Mito6211 <willmoore674@gmail.com>
2021-03-14 15:26:20 -0400

    Added old changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './App.css';
+import styles from "./App.module.css";
 
 export default function App() {
-
     const [formData, setFormData] = useState({
-        note: localStorage.getItem("note") || ""
+        note: localStorage.getItem("note") || "",
+        allNotes: JSON.parse(localStorage.getItem("allNotes")) || [],
     });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setFormData(prevData => ({
+        setFormData((prevData) => ({
             ...prevData,
-            [name]: value
+            [name]: value,
         }));
-    }
+    };
+
+    const addNote = () => {
+        setFormData((prevData) => ({
+            ...prevData,
+            note: "",
+            allNotes: [...prevData.allNotes, formData.note],
+        }));
+        localStorage.setItem("allNotes", JSON.stringify([...formData.allNotes, formData.note]));
+
+    };
 
     useEffect(() => {
-        localStorage.setItem("note", formData.note)
-    }, [formData.note])
+        localStorage.setItem("note", formData.note);
+    }, [formData.note]);
 
     return (
-        <div>
-            <textarea
-                placeholder="add a note here!"
-                name="note"
-                value={formData.note}
-                onChange={handleChange}
-            />
+        <div className={styles.notesContainer}>
+            <div className={styles.addNoteItems}>
+                <textarea
+                    className={styles.addNoteField}
+                    placeholder="add a note here!"
+                    name="note"
+                    value={formData.note}
+                    onChange={handleChange}
+                />
+                <button className={styles.addNoteButton} onClick={addNote}>
+                    Add Note
+                </button>
+            </div>
+            <div>
+                {formData.allNotes.map((note) =>
+                    <div className={styles.allNotes}>
+                        <p>{note}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
