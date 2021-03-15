7b36c93c424d82d5a039fff981e7f8a3495f7fcc
Mito6211 <willmoore674@gmail.com>
2021-03-14 21:11:00 -0400

    Added react router, notes page

--- src/App.js
+++ src/App.js
@@ -8,10 +8,19 @@ import {
   Textarea,
   Heading,
   Spacer,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
 } from "@chakra-ui/react";
+import { Switch, Route, useHistory, useLocation } from "react-router-dom";
 import { MdClose } from "react-icons/md";
 
 export default function App() {
+  const history = useHistory();
+  const location = useLocation();
   const [note, setNote] = useState(localStorage.getItem("note") || "");
   const [allNotes, setAllNotes] = useState(
     JSON.parse(localStorage.getItem("allNotes")) || []
@@ -29,49 +38,84 @@ export default function App() {
           <Heading size="lg">Notes Demo</Heading>
         </Box>
         <Spacer />
-        <Button colorScheme="orange">View Notes</Button>
-      </Flex>
-      <Flex direction="column">
-        <Textarea
-          minH="300px"
-          placeholder="Add a note here!"
-          name="note"
-          value={note}
-          onChange={(e) => setNote(e.target.value)}
-        />
         <Button
-          onClick={() => {
-            setAllNotes((prev) => [...prev, note]);
-            setNote("");
-          }}
           colorScheme="orange"
-          my="10px"
+          onClick={() =>
+            history.push(location.pathname !== "/notes" ? "/notes" : "/")
+          }
         >
-          Add Note
+          {location.pathname !== "/notes" ? "View Notes" : "Back"}
         </Button>
       </Flex>
-      <Flex direction="column">
-        {allNotes.map((note, idx) => (
-          <Flex my="5px" key={idx} align="center">
+      <Switch>
+        <Route exact path="/">
+          <Flex direction="column">
+            <Textarea
+              minH="300px"
+              placeholder="Add a note here!"
+              name="note"
+              value={note}
+              onChange={(e) => setNote(e.target.value)}
+            />
             <Button
-              colorScheme="red"
-              variant="outline"
-              mr="20px"
-              onClick={() =>
-                setAllNotes((prev) => prev.filter((note) => prev[idx] !== note))
-              }
+              onClick={() => {
+                setAllNotes((prev) => [...prev, note]);
+                setNote("");
+              }}
+              colorScheme="orange"
+              my="10px"
             >
-              <MdClose />
+              Add Note
             </Button>
-            <Text>{note}</Text>
           </Flex>
-        ))}
-        {allNotes.length > 0 && (
-          <Button colorScheme="red" onClick={() => setAllNotes([])} my="10px">
-            Delete All Notes
-          </Button>
-        )}
-      </Flex>
+          <Flex direction="column">
+            {allNotes.map((note, idx) => (
+              <Flex my="5px" key={idx} align="center">
+                <Button
+                  colorScheme="red"
+                  variant="outline"
+                  mr="20px"
+                  onClick={() =>
+                    setAllNotes((prev) =>
+                      prev.filter((note) => prev[idx] !== note)
+                    )
+                  }
+                >
+                  <MdClose />
+                </Button>
+                <Text>{note}</Text>
+              </Flex>
+            ))}
+            {allNotes.length > 0 && (
+              <Button
+                colorScheme="red"
+                onClick={() => setAllNotes([])}
+                my="10px"
+              >
+                Delete All Notes
+              </Button>
+            )}
+          </Flex>
+        </Route>
+        <Route path="/notes">
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Number</Th>
+                <Th>Note</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {allNotes.map((note, idx) => (
+                <Tr key={idx}>
+                  <Td>{idx + 1}</Td>
+                  <Td>{note}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Route>
+      </Switch>
     </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
@@ -8,7 +9,9 @@ import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
